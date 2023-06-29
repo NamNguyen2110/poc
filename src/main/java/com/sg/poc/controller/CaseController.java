c8c309e0bc7af1b9749b18009612cb27f154c5e9
HoangPT <phantichhoang@gmail.com>
2023-06-29 14:41:24 +0700

    feat: suggestion

--- src/main/java/com/sg/poc/controller/CaseController.java
+++ src/main/java/com/sg/poc/controller/CaseController.java
@@ -5,28 +5,16 @@ import com.sg.poc.domain.dto.IngestRequest;
 import com.sg.poc.service.LawInjuryCaseService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/poc/v1")
 @RequiredArgsConstructor
 @Slf4j
-public class ElasticSearchController {
+public class CaseController {
 
   private final LawInjuryCaseService injuryCaseService;
-
-  @PostMapping(value = "/cases")
-  public ResponseEntity<ApiResponse<Object>> ingest(@RequestBody IngestRequest request) {
-    return ResponseEntity.ok(ApiResponse.success(injuryCaseService.ingest(request)));
-  }
 
   @GetMapping(value = "/cases")
   public ResponseEntity<ApiResponse<Object>> findById(@RequestParam("id") Integer id) {
---
+++ src/main/java/com/sg/poc/controller/ETLController.java
@@ -0,0 +1,28 @@
+package com.sg.poc.controller;
+
+import com.sg.poc.domain.dto.ApiResponse;
+import com.sg.poc.domain.dto.IngestRequest;
+import com.sg.poc.service.LawInjuryCaseService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/poc/v1")
+@RequiredArgsConstructor
+@Slf4j
+public class ETLController {
+
+  private final LawInjuryCaseService injuryCaseService;
+
+  @PostMapping(value = "/cases")
+  public ResponseEntity<ApiResponse<Object>> ingest(@RequestBody IngestRequest request) {
+    return ResponseEntity.ok(ApiResponse.success(injuryCaseService.ingest(request)));
+  }
+}
